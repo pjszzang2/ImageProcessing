b7e5c01827d60b40f5e16363dd70c8cc578b0c15
pjszzang2 <pjszzang2@naver.com>
2017-11-10 22:31:36 -0800

    make manager dir and main source file

---
+++ ImageProcessing/pmanager/pmanager.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+	printf("This file is Program Manager file. We call not only library and filter but also gui library\n");
+	printf("This file is main execute file\n");
+	
+}
