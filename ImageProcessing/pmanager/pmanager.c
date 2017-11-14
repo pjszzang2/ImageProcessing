99016e61e249c546d950d0363fefb902f63fdb32
pjszzang2 <pjszzang2@naver.com>
2017-11-14 06:18:03 -0800

    add header in pmanager source file

--- ImageProcessing/pmanager/pmanager.c
+++ ImageProcessing/pmanager/pmanager.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+#include"filter.h"
+
 int main(){
 	printf("This file is Program Manager file. We call not only library and filter but also gui library\n");
 	printf("This file is main execute file\n");
