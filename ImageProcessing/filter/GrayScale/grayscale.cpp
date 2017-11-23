ebda67614a76b0702572621dcdea806e86891811
DooDoo <grassleaft@gmail.com>
2017-11-23 18:43:28 +0900

    Grayscale

---
+++ ImageProcessing/filter/GrayScale/grayscale.cpp
@@ -0,0 +1,15 @@
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+using namespace cv;
+
+int main()
+{
+   Mat ori_img, gray_img;
+   ori_img = imread("/home/doo/ImageProcessing/ImageProcessing/img/atj.jpg", IMREAD_COLOR);
+   cvtColor(ori_img, gray_img, COLOR_BGR2GRAY);
+   imshow("Original Image", ori_img);
+   imshow("Grayscale Image", gray_img);
+   while(cvWaitKey(0)==0);
+return 0;
+}
