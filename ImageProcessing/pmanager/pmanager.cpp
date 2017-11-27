f8e2a602c3ebc826a805d0290f0a96651eb47bb7
DooDoo <grassleaft@gmail.com>
2017-11-27 16:56:51 +0900

    pmanager MK.1

--- ImageProcessing/pmanager/pmanager.cpp
+++ ImageProcessing/pmanager/pmanager.cpp
@@ -1,29 +1,52 @@
 #include <iostream>
+//#include <Grayscale.h>
+//#include <Rotate.h>
+//#include <Blah.h>
 
 using namespace std;
 
-class PManager{
+
+class Filter{
+private:
+int rd;
+public:
+   void Rotate(){
+      cout<<"[ Rotate ]"<<endl;
+      cout <<"Enter the Rotate Degree : ";
+      cin >> rd;
+   }
+   void Grayscale(){
+      cout<<"[ Grayscale ]"<<endl;
+   }
+};
+
+class PManager : Filter{
 private:
    int fnum;
-
 public:
-   PManager(){
-      cout <<"Choose a filter you want. "<< endl;
-      cout <<"1.Rotate   2.Grayscale   3.blah blah ..." <<endl;
+   PManager(){}
+   int Get_Fnum(){return fnum;}
+   void Op_Input(){
+      cout << endl << endl << endl << "<< Choose a filter you want. >>"<< endl;
+      cout <<"0.Program Termination   1.Rotate   2.Grayscale   3.blah blah ..." <<endl<<endl;
       cin >> fnum;
-   }
-   void Op_Fnum(){
       switch(fnum){
-         case 1: cout<<"Rotate"<<endl; break;
-         case 2: cout<<"Grayscale"<<endl; break;
+         case 1: Rotate(); break;
+         case 2: Grayscale(); break;
+         case 3: cout<<"Blah Blah"<<endl; break;
          default : break;
       }
    }
 };
 
+
 int main(){
    PManager PM;
-   PM.Op_Fnum();
+   int termin=1;
+   while(termin!=0){
+      PM.Op_Input();
+      termin=PM.Get_Fnum();
+   }
 
 return 0;
 }
