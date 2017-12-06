89d01499e52e26f52b448ff96052484406bf3872
pjszzang2 <pjszzang2@naver.com>
2017-12-05 21:15:58 -0800

    modify the header

--- ImageProcessing/lib/filter.h
+++ ImageProcessing/lib/filter.h
@@ -1,12 +1,21 @@
 #ifndef FILTER_H
 #define FILTER_H
 
-void brightness();
-void grayscale();
-void leftRotate();
-void rightRotate();
-void leftright();
-void updown();
+#include <opencv/cv.h>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+#include <opencv2/core/core.hpp>
+
+using namespace cv;
+using namespace std;
+
+
+void brightness(Mat ori_img);
+void grayscale(Mat ori_img);
+void rotate(Mat ori_img);
+void sharpening(Mat img);
+void sobel(Mat ori_img);
 
 #endif
 
